99eb064a5d84f1f8802ecf62b6c23e1ec3145e23
Jonathan Berney <jberney@pivotal.io>
2017-09-27 15:34:51 -0700

    make the styleguide static, and add v8 guide to old versions [#151462438]
    make the styleguide static, and add v8 guide to old versions [#151462438]

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ import '../stylesheets/app.scss';
 
 import Sidebar from './components/sidebar';
 import MarkdownViewer from './components/markdown_viewer';
-import TocSidebar from './components/toc_sidebar';
 import contentMap, {attachPackagesToWindow} from './helpers/content';
 import 'pivotal-ui/js/prismjs';
 
@@ -15,19 +14,19 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    const path = window.location.pathname;
+    const path = window.location.pathname.split('/').pop();
     this.state = {content: App.currentContent(path), path: path};
   }
 
   updateContent(href) {
     window.history.pushState({}, '', href);
     content.scrollTop = 0;
-    const path = window.location.pathname;
+    const path = window.location.pathname.split('/').pop();
     this.setState({content: App.currentContent(path), path: path});
   }
 
   static currentContent(path) {
-    return contentMap[path] ? contentMap[path] : contentMap['/404'];
+    return contentMap[path] || contentMap['404'];
   }
 
   render() {
