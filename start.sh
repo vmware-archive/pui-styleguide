d04f12ba3f12e9d219c075f03cbbf147875935df
Reid Mitchell <rmitchell@pivotal.io>
2017-09-27 13:37:03 -0700

    make styleguide a static app [#151462438]
    Signed-off-by: Jonathan Berney <jberney@pivotal.io>

--- start.sh
+++ start.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 
-printf 'Rebuild Pivotal UI library code? [y/N]: '
-read input
-
-if [[ "$input" == "y" || "$input" == "Y" ]]; then
-  pushd ../pivotal-ui
-    yarn install --no-progress
-    gulp build
-  popd
-fi
+pushd ../pivotal-ui
+  yarn install --no-progress
+  gulp build
+popd
 
 rm -rf node_modules
-yarn install --no-progress
-./node_modules/.bin/webpack --config ./backend_webpack.config.babel.js --progress -p
-./node_modules/.bin/webpack --config ./frontend_webpack.config.babel.js --progress -p
+yarn cache clean
+yarn --no-progress
 
-npm run watch
+rm -rf dist
+./node_modules/.bin/webpack --config ./frontend_webpack.config.babel.js -p
+
+cp public/* dist/
+cp -R static dist/
