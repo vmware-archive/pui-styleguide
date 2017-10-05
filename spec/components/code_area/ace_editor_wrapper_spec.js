bd4e31425235b168f98efe86388405bd8720961c
Ming Xiao <mxiao@pivotal.io>
2017-10-05 10:23:21 -0700

    test and server running

--- spec/components/code_area/ace_editor_wrapper_spec.js
+++ spec/components/code_area/ace_editor_wrapper_spec.js
@@ -2,7 +2,6 @@ import '../../spec_helper';
 import AceEditor from 'react-ace';
 import AceEditorWrapper from '../../../src/components/code_area/ace_editor_wrapper';
 
-
 describe('AceEditorWrapper', () => {
   let code, changeHandler;
 
@@ -13,11 +12,6 @@ describe('AceEditorWrapper', () => {
 
     ReactDOM.render(<AceEditorWrapper {...{code, changeHandler}}/>, root);
   });
-
-  // it('renders an editor', () => {
-  //   expect(".code-editor--edit").toExist();
-  // });
-
 
   it('renders the AceEditor with the correct props', () => {
     expect(AceEditor).toHaveBeenRenderedWithProps({
--- tasks/jasmine.js
+++ tasks/jasmine.js
@@ -6,18 +6,17 @@ Jasmine.install({
   browserSpecRunnerOptions: {sourcemappedStacktrace: true, profile: false},
   headlessSpecRunnerOptions: {},
   headlessServerOptions: {driver: 'phantomjs', random: false, profile: false},
-  appGlobs: ['spec/**/*spec.js'],
   serverOptions: {config: {random: false}},
   webpack: {
     test: () => {
       return {
-        // entry: {spec: './spec/**/*spec.js'},
+        entry: {spec: './spec/index.js'},
         module: {
           rules: [
-            {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'},
+            {test: /\.jsx?$/, exclude: /node_modules/, use: ['babel-loader']},
           ]
         },
-        output: {filename: '[name].js'}
+        output: {filename: '[name].js', chunkFilename: '[id].js'}
       }
     }
   }
